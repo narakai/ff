26c2a9ac7bcb1ea5001283afc6b1955092c25750
laileon <leon@igola.com>
2018-04-11 16:10:05 +0800

    settings

--- testPluginJar/src/main/java/com/rogery/testpluginjar/Constants.java
+++ testPluginJar/src/main/java/com/rogery/testpluginjar/Constants.java
@@ -15,12 +15,13 @@ public class Constants {
     public static final String F_NUM = "flight_num";//航班号
     public static final String CKI_RESULT = "CkiResult";//值机结果
     public static final String THIRD_USER_ID = "third_user_id";//第三方用户标示
-    public static final String AIRPORT_CODE = "airport_code";//机场三字码
+    public static final String AIRPORT_CODE = "airport_code";//机场三字码`
     public static final String L_ZH = "l_zh";//中英文,0，中文；1英文  暂不支持英文
 
-    public static  String appid = "liuhao";
-    public static String appkey = "000000";
+    public static  String appid = "ume_85c33204d0dd4dfd801f88c1c876f7fa";
+    public static String appkey = "5677d222b2211010e21b7051a39cdd82";
+//    3082034930820231a0030201020204
 
-    public static String userIdStr = "user1";
-    public static String userTokenStr = "liuhao";
+    public static String userIdStr = "igola1";
+    public static String userTokenStr = "igola";
 }
